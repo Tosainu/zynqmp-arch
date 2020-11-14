a25842b789ceed86a7f3b006b439b537adb275d6
Kenta Sato <tosainu.maple@gmail.com>
2020-11-14 15:19:00 +0900

    Prevent unneeded package rebuilding

--- ci/packages.sh
+++ ci/packages.sh
@@ -6,23 +6,48 @@ sudo pacman -Syu --noconfirm
 
 sudo sed -i "s/^#\(MAKEFLAGS=\).*/\1\"-j$(( $(nproc) * 2 ))\"/" /etc/makepkg.conf
 
+# another pacman conig and db for downloading previously built packages
+cp /etc/pacman.conf ~/
+cat >> ~/pacman.conf <<EOS
+
+[zynqmp-arch]
+SigLevel = Never
+Server = https://zynqmp-arch.myon.info/\$arch
+EOS
+
+cp -r /var/lib/pacman ~/db
+fakeroot pacman -Sy --noconfirm --noprogressbar --config ~/pacman.conf --dbpath ~/db
+
+dlpkg() {
+  fakeroot pacman -Sddw --noconfirm --noprogressbar --config ~/pacman.conf --dbpath ~/db --cachedir . $@
+}
+
 sudo mkdir /work
 sudo chown $(id -un):$(id -gn) /work
 cd /work
 
 cp -r /repo/PKGBUILDs .
 
-pushd PKGBUILDs/linux-zynqmp/
-makepkg -si --noconfirm --noprogressbar
-popd
+for p in \
+  PKGBUILDs/linux-zynqmp \
+  PKGBUILDs/wilc3000-ultra96v2 \
+  PKGBUILDs/wilc-firmware
+do
+  pushd "$p"
 
-pushd PKGBUILDs/wilc3000-ultra96v2/
-makepkg -si --noconfirm --noprogressbar
-popd
+  mapfile -t pkg_names < <(makepkg --printsrcinfo | grep -Po '(?<=pkgname = ).+')
+  dlpkg "${pkg_names[@]}" || true
 
-pushd PKGBUILDs/wilc-firmware/
-makepkg -si --noconfirm --noprogressbar
-popd
+  mapfile -t pkg_files < <(makepkg --packagelist)
+  if ! ls "${pkg_files[@]}" > /dev/null 2>&1; then
+    rm -f ./*.pkg.tar.xz
+    makepkg -si --noconfirm --noprogressbar
+  else
+    sudo pacman -U --noconfirm --noprogressbar "${pkg_files[@]}"
+  fi
+
+  popd
+done
 
 mkdir pkgs
 mv PKGBUILDs/*/*.pkg.tar.xz pkgs/
